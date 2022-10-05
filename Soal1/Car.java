199f794e1367d684a5d90563a39c157637264553
Mas-Azi <112527000+Mas-Azi@users.noreply.github.com>
2022-10-05 18:33:20 +0700

    Add files via upload

---
+++ Soal1/Car.java
@@ -0,0 +1,16 @@
+package Soal1;
+public class Car {
+    int modelYear;
+    String modelName;
+
+    public Car(int year, String name){
+        modelYear= year;
+        modelName = name;
+    }
+
+    public static void main(String[] args) {
+        Car mycar = new Car(1998,"Mustang");
+        System.out.println(mycar.modelYear +" "+ mycar.modelName);
+
+    }
+}
